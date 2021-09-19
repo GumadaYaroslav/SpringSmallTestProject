b97faf937971b0427a199eb3ba89de80127e7ee9
Free Milissa <fmilissa@ge-h4.kzn.21-school.ru>
2021-09-19 22:38:13 +0300

    Factory method

--- src/main/java/org/example/RockMusic.java
+++ src/main/java/org/example/RockMusic.java
@@ -1,6 +1,12 @@
 package org.example;
 
 public class RockMusic implements Music {
+    private RockMusic() {}
+
+    public static RockMusic getRockMusic() {
+        return new RockMusic();
+    }
+
     public  void doMyInit() {
         System.out.println("Doing my initialization");
     }
